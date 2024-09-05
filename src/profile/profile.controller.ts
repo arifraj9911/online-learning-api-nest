d1cfa53365fb85f34eede394c46449dd1d9cf2a0
Arif Hossain <arif.raj9911@gmail.com>
2024-09-05 21:21:18 +0600

    role based login with jwt implement

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/RBAC/roles.guard';
 
 @Controller('profile')
 export class ProfileController {
   @Get('/teacher')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @SetMetadata('roles', ['teacher'])
   getTeacher(@Request() req: any) {
     return req.user;
   }
   @Get('/student')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @SetMetadata('roles', ['student'])
   getStudent(@Request() req: any) {
     return req.user;
   }
